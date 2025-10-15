9e20337ec0009c0d4ce7448edb75870fcb2ae647
Sama DKT <dsama31@gmail.com>
2025-10-15 14:46:50 +0200

    First commit

---
+++ src/showConsole.js
@@ -0,0 +1,13 @@
+//Initialisation d'une variable
+let a = "Hello";
+
+//Initialisation d'un tableau
+let tab = [];
+
+//Ajout valeur dans un tableau
+tab.push(a);
+
+//Visualisation
+console.log("Valeur de la variable 'a' " + a);
+console.log("Affichage de l'objet 'tab' : " + tab);
+console.log("Affichage de la valeur à l'indice 0 de 'tab' : " + tab[0]);
